3a94f38e731b2dae537d5ca1470b5f7ef105a375
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-03-29 11:59:52 +0100

    Ajout de la page risques

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main_routes = Blueprint('main', __name__, static_folder="static", template_folde
 @main_routes.route('/')
 def index():
     return render_template('index.html')
+
+@main_routes.route('/risques')
+def risques():
+    return render_template('risques.html')
